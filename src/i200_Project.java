82332a4e7b5ed19359fb19dcbff68a71d8cb35d1
Svetlana Jugai <nyah88@gmail.com>
2016-10-06 08:05:29 +0300

    i200 projekt

---
+++ src/i200_Project.java
@@ -0,0 +1,10 @@
+/**
+ * Created by svetlana on 6.10.16.
+ */
+public class i200_Project {
+    public static void main(String[] args) {
+        System.out.println("i200 Projektiks on laulmise harjutused.");
+        System.out.println("Autor on Svetlana Jugai.");
+        System.out.println("Alustatud 6. oktoobril 2016.");
+    }
+}
