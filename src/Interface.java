90d7e94bd6975eb992e881506903c5e1f3525c63
Svetlana Jugai <nyah88@gmail.com>
2016-12-26 07:59:35 +0200

    Labels to interface added! Wohhoo! Why didn't it work before?..

--- src/Interface.java
+++ src/Interface.java
@@ -2,8 +2,11 @@ import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 /**
  * Created by svetlana on 10.12.16.
@@ -22,21 +25,27 @@ public class Interface {
         primaryStage.show();
 
 
-        //add label that says "Please select your sex"
+        Label label1 = new Label("Please select your sex");
         ChoiceBox selection1 = new ChoiceBox(FXCollections.observableArrayList("Man", "Woman"));
         pane.getChildren().add(selection1);
+        pane.getChildren().add(label1);
+        label1.setLayoutX(80);
         selection1.setLayoutX(100);
-
+        selection1.setLayoutY(20);
 
         //add label that says "What type of exercise do you want?"
+        Label label2 = new Label("What type of exercise do you want?");
         ChoiceBox selection2 = new ChoiceBox(FXCollections.observableArrayList("Warm up", "Breathing", "High notes"));
         pane.getChildren().add(selection2);
+        pane.getChildren().add(label2);
+        label2.setLayoutX(40);
+        label2.setLayoutY(70);
         selection2.setLayoutX(90);
-        selection2.setLayoutY(50);
+        selection2.setLayoutY(90);
 
         Button btn = new Button("Start singing!");
-        btn.setLayoutX(90);
-        btn.setLayoutY(100);
+        btn.setLayoutX(95);
+        btn.setLayoutY(150);
         pane.getChildren().add(btn);
 
         selection1.setOnAction(event -> {
